abf012d2479f8e2ccc44826469d27e211607a09b
sho-hiwada <hiwasho@icloud.com>
2023-04-04 16:26:50 +0900

    Initial commit.

---
+++ netflix/pages/watch/[movieId].tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { useRouter } from 'next/router';
+import useMovie from '@/hooks/useMovie';
+
+
+const Watch = () => {
+  const router = useRouter();
+  console.log(router.query);
+  const { movieId } = router.query;
+
+  const { data } = useMovie(movieId as string);
+  console.log(data);
+  return (
+    <div className='h-screen w-screen bg-black'>
+        <nav className='
+        fixed
+        w-full
+        p-4
+        z-10
+        flex
+        flex-row
+        items-center
+        gap-8
+        bg-black
+        bg-opacity-70
+        '>
+          <AiOutlineArrowLeft
+          onClick={() => router.push(`/`)}
+          className="cursor-pointer text-white"
+          size={25}
+          />
+          <p className='text-white text-1xl md:text-3xl font-bold'>
+            <span className='font-light'>
+              タイトル：
+            </span>
+            {data?.title}
+          </p>
+        </nav>
+      <video
+      autoPlay
+      controls
+      className='h-full w-full'
+      src={data?.videoUrl}
+      ></video>
+    </div>
+  )
+}
+
+export default Watch;
+
